c9c1b4a26962cf3a1e6dfaa1808486d0974e7465
Ryan Smith <ryan_smith2@hotmail.com>
2020-07-31 16:47:39 -0500

    added adjective, pluralNoun, superHero string variables. CW/CR for user input. Simply made this to refresh some basic c# skills.
    git add .

--- MadLibs/Program.cs
+++ MadLibs/Program.cs
@@ -10,6 +10,22 @@ namespace MadLibs
     {
         static void Main(string[] args)
         {
+            string adjective, pluralNoun, superHero;
+
+            Console.WriteLine("Enter a adjective: ");
+            adjective = Console.ReadLine();
+
+            Console.WriteLine("Enter a plural noun: ");
+            pluralNoun= Console.ReadLine();
+
+            Console.WriteLine("Enter a super hero: ");
+            superHero = Console.ReadLine();
+
+            Console.WriteLine("Roses are " + adjective);
+            Console.WriteLine(pluralNoun + " are blue");
+            Console.WriteLine("I hate " + superHero);
+
+            Console.ReadLine();
         }
     }
 }
